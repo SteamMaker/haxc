375c6d4652f2308282e4a74cec3f70737e61fd23
Bret Stateham <Bret.Stateham@microsoft.com>
2015-10-01 11:10:31 -0700

    Added dependency on Remote-Wiring and Firmata
    Added references to the Remote-Wiring libraries and am able to use it to
    control the Arduino and read values from it.  W00T!

--- HAXCSolar/HAXCSolar/MainPage.xaml.cs
+++ HAXCSolar/HAXCSolar/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Maker.RemoteWiring;
+using Microsoft.Maker.Serial;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -17,14 +19,68 @@ using Windows.UI.Xaml.Navigation;
 
 namespace HAXCSolar
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
+  /// <summary>
+  /// An empty page that can be used on its own or navigated to within a Frame.
+  /// </summary>
+  public sealed partial class MainPage : Page
+  {
+    IStream connection;
+    RemoteDevice arduino;
+    DispatcherTimer timer;
+    bool arduinoReady = false;
+
+    public MainPage()
     {
-        public MainPage()
-        {
-            this.InitializeComponent();
-        }
+      this.InitializeComponent();
+
+      connection = new UsbSerial("VID_2341", "PID_0043");
+      arduino = new RemoteDevice(connection);
+      arduino.DeviceReady += Arduino_DeviceReady;
+
+
+      connection.begin(57600, SerialConfig.SERIAL_8N1);
+
+      timer = new DispatcherTimer();
+      timer.Interval = TimeSpan.FromMilliseconds(100);
+      timer.Tick += Timer_Tick;
+      timer.Start();
     }
+
+    private void Timer_Tick(object sender, object e)
+    {
+      if(arduinoReady)
+      {
+        UInt16 val = arduino.analogRead("A0");
+        sliderArray01.Value = val;
+      }
+    }
+
+    private void Arduino_DeviceReady()
+    {
+      //TODO
+      var action = Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, new Windows.UI.Core.DispatchedHandler(() =>
+     {
+       OnButton.IsEnabled = true;
+       OffButton.IsEnabled = true;
+     }));
+
+      //set digital pin 13 to OUTPUT
+      arduino.pinMode(13, PinMode.OUTPUT);
+
+      //set analog pin A0 to ANALOG INPUT
+      arduino.pinMode("A0", PinMode.ANALOG);
+
+      arduinoReady = true;
+    }
+
+    private void OnButton_Click(object sender, RoutedEventArgs e)
+    {
+      arduino.digitalWrite(13, PinState.HIGH);
+    }
+
+    private void OffButton_Click(object sender, RoutedEventArgs e)
+    {
+      arduino.digitalWrite(13, PinState.LOW);
+    }
+  }
 }
